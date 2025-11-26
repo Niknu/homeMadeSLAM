9bf952b7aeecf5b757c1e859a2eb9674a4f1ebef
Niknu <nicolai.hk@gmail.com>
2025-11-26 14:59:57 +0100

    Can read from the file and convert it to homogenous matrix

--- src/helper_nodes/helper_nodes/kitti2ros.py
+++ src/helper_nodes/helper_nodes/kitti2ros.py
@@ -1,29 +1,75 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Vector3
+
+import pathlib
+import numpy as np
 
 
 class MinimalPublisher(Node):
-    def __init__(self):
-        super().__init__("minimal_publisher")
-        self.publisher_ = self.create_publisher(String, "topic", 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+    def __init__(self, kitti_path, seqence_no):
+        super().__init__("kitti_publisher")
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = "Hello World: %d" % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        self.seqence_no = seqence_no
+        gt_file_seqence_no = self.seqence_no + ".txt"
+
+        self.kitti_folder = pathlib.Path(kitti_path)
+        self.kitti_folder_gt = (
+            self.kitti_folder
+            / "data_odometry_poses"
+            / "dataset"
+            / "poses"
+            / gt_file_seqence_no
+        )
+        self.kitti_folder_images = (
+            self.kitti_folder
+            / "data_odometry_color"
+            / "dataset"
+            / "sequences"
+            / self.seqence_no
+            / "image_2"
+        )
+
+        self.gt_file_line_no = 0
+        timer_period = 0.1  # seconds
+        #        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.get_ground_truth_pos()
+
+    def get_new_image(self):
+        pass
+
+    def get_new_timestamp(self):
+        pass
+
+    def get_ground_truth_pos(self):
+        # data_odometry_poses/dataset/poses/<sequenceNO>.txt
+        # format: 3x4 rotmatix r11 r12 r13 t1 r21 r22 r23 t2 r31 r32 r33 t3
+
+        gt_text = self.read_line(self.kitti_folder_gt, self.gt_file_line_no)
+        print(gt_text)
+        row = list(map(float, gt_text.split()))
+        T = np.array(row).reshape(3, 4)
+        print(T)
+        T_hom = np.vstack([T, [0, 0, 0, 1]])
+        print(T_hom)
+
+    def read_line(self, path, line_no) -> str:
+        with open(path, "r") as file:
+            for i, line in enumerate(file):
+                if i == line_no:
+                    return line.rstrip()
+
+    # def timer_callback(self):
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    kitti_path = "/workspace/src/KITTI_data"
+    minimal_publisher = MinimalPublisher(kitti_path, seqence_no="00")
 
     rclpy.spin(minimal_publisher)
 
